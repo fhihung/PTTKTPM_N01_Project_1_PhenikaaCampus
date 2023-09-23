d0d4d33e516389a3a81ef16b129906e5eb14421f
Nguyen Phi Hung <dev.phihung@gmail.com>
2023-09-23 18:08:47 +0700

    update color to no deprecated

--- phenikaa_campus/lib/main.dart
+++ phenikaa_campus/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phenikaa_campus/common/common.dart';
+import 'package:phenikaa_campus/common/error_page.dart';
 import 'package:phenikaa_campus/features/auth/controller/auth_controller.dart';
 import 'package:phenikaa_campus/features/auth/view/login_view.dart';
 import 'package:phenikaa_campus/features/auth/view/signup_view.dart';
-import 'package:phenikaa_campus/features/home/view/home_view.dart';
 import 'package:phenikaa_campus/theme/theme.dart';
 
 void main() {
