69a3c1c52d47876d043585347be585ba5f2c95c8
QuangViDang <vidangquang1203@gmail.com>
2023-09-26 17:36:32 +0700

    Simple List tweet

---
+++ phenikaa_campus/lib/features/tweet/widgets/tweet_card.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:phenikaa_campus/constants/assets_constants.dart';
+import 'package:phenikaa_campus/features/auth/controller/auth_controller.dart';
+import 'package:phenikaa_campus/models/tweet_model.dart';
+
+import '../../../common/error_page.dart';
+import '../../../common/loading_page.dart';
+import '../../../theme/pallete.dart';
+
+class TweetCard extends ConsumerWidget {
+  final Tweet tweet;
+  const TweetCard({
+    super.key,
+    required this.tweet,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+
+    return currentUser == null
+        ? const SizedBox()
+        : ref.watch(userDetailsProvider(tweet.uid)).when(
+              data: (user) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.all(10),
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(user.profilePic),
+                                radius: 15,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(
+                                        right: user.isTwitterBlue ? 1 : 5,
+                                      ),
+                                      child: Text(
+                                        user.name,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                    if (user.isTwitterBlue)
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 5.0),
+                                        child: SvgPicture.asset(
+                                          AssetsConstants.verifiedIcon,
+                                        ),
+                                      ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SvgPicture.asset(
+                                      AssetsConstants.retweetIcon,
+                                      color: Pallete.greyColor,
+                                      height: 20,
+                                    ),
+                                    const SizedBox(width: 2),
+                                    Text(
+                                      '${tweet.text} ',
+                                      style: const TextStyle(
+                                        color: Pallete.greyColor,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(color: Pallete.greyColor),
+                    ],
+                  ),
+                );
+              },
+              error: (error, stackTrace) => ErrorText(
+                error: error.toString(),
+              ),
+              loading: () => const Loader(),
+            );
+  }
+}
