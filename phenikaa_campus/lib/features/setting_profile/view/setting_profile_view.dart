8e8523095dd17d3b4887f5785025ec96ec6d886f
Dang Quang Vi <vidangquang1203@gmail.com>
2023-11-04 10:04:30 +0700

    Duong fix D, Q fix Q

--- phenikaa_campus/lib/features/setting_profile/view/setting_profile_view.dart
+++ phenikaa_campus/lib/features/setting_profile/view/setting_profile_view.dart
@@ -28,147 +28,166 @@ class _SettingProfileViewState extends ConsumerState<SettingProfileView> {
     final currentUser = ref.watch(currentUserDetailsProvider);
     const posi1 = 100.0;
     return switch (currentUser) {
-      AsyncData(value: final currentUser?) || AsyncLoading(value: final currentUser?) => Scaffold(
+      AsyncData(value: final currentUser?) ||
+      AsyncLoading(value: final currentUser?) =>
+        Scaffold(
           body: Stack(
             children: [
               Positioned.fill(child: Image.asset(AssetsConstants.darkBlur)),
               Positioned.fill(
-                child: Column(
-                  children: [
-                    DecoratedBox(
-                      decoration: BoxDecoration(
-                        gradient: Pallete.cardColor,
-                        borderRadius: BorderRadius.circular(24.0),
+                child: SafeArea(
+                  top: false,
+                  minimum: EdgeInsets.only(bottom: 30),
+                  child: Column(
+                    children: [
+                      DecoratedBox(
+                        decoration: BoxDecoration(
+                          gradient: Pallete.cardColor,
+                          borderRadius: BorderRadius.circular(24.0),
+                        ),
+                        child: const SizedBox(
+                            height: 300, width: double.maxFinite),
                       ),
-                      child: const SizedBox(height: 300, width: double.maxFinite),
-                    ),
-                    Expanded(
-                      child: OverflowPadding(
-                        padding: const EdgeInsets.only(top: -30, bottom: -30),
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: Pallete.rhinoDark700,
-                            borderRadius: BorderRadius.circular(24.0),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Center(
-                                child: OverflowPadding(
-                                  padding: const EdgeInsets.only(
-                                    top: -posi1,
-                                  ),
-                                  child: Container(
-                                    padding: const EdgeInsets.all(5),
-                                    decoration: const ShapeDecoration(
-                                      color: Pallete.rhinoDark800,
-                                      shape: CircleBorder(),
+                      Expanded(
+                        child: OverflowPadding(
+                          padding: const EdgeInsets.only(top: -30, bottom: -30),
+                          child: DecoratedBox(
+                            decoration: BoxDecoration(
+                              color: Pallete.rhinoDark700,
+                              borderRadius: BorderRadius.circular(24.0),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Center(
+                                  child: OverflowPadding(
+                                    padding: const EdgeInsets.only(
+                                      top: -posi1,
                                     ),
-                                    constraints: BoxConstraints.tight(const Size.square(160)),
-                                    child: Stack(
-                                      fit: StackFit.loose,
-                                      children: [
-                                        CircleAvatar(backgroundImage: NetworkImage(currentUser.profilePic), radius: 80),
-                                        Align(
-                                          alignment: Alignment.bottomRight,
-                                          child: Container(
-                                            padding: const EdgeInsets.all(5),
-                                            decoration: const ShapeDecoration(
-                                              color: Pallete.rhinoDark800,
-                                              shape: CircleBorder(),
-                                            ),
-                                            child: CircleAvatar(
-                                              backgroundColor: Pallete.yellow800,
-                                              radius: 25,
-                                              child: IconButton(
-                                                onPressed: () {},
-                                                icon: const Icon(
-                                                  Icons.camera_alt,
-                                                  color: Pallete.rhinoDark800,
+                                    child: Container(
+                                      padding: const EdgeInsets.all(5),
+                                      decoration: const ShapeDecoration(
+                                        color: Pallete.rhinoDark800,
+                                        shape: CircleBorder(),
+                                      ),
+                                      constraints: BoxConstraints.tight(
+                                          const Size.square(160)),
+                                      child: Stack(
+                                        fit: StackFit.loose,
+                                        children: [
+                                          CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  currentUser.profilePic),
+                                              radius: 80),
+                                          Align(
+                                            alignment: Alignment.bottomRight,
+                                            child: Container(
+                                              padding: const EdgeInsets.all(5),
+                                              decoration: const ShapeDecoration(
+                                                color: Pallete.rhinoDark800,
+                                                shape: CircleBorder(),
+                                              ),
+                                              child: CircleAvatar(
+                                                backgroundColor:
+                                                    Pallete.yellow800,
+                                                radius: 25,
+                                                child: IconButton(
+                                                  onPressed: () {},
+                                                  icon: const Icon(
+                                                    Icons.camera_alt,
+                                                    color: Pallete.rhinoDark800,
+                                                  ),
                                                 ),
                                               ),
                                             ),
-                                          ),
-                                        )
-                                      ],
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              PaddedRow(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 15,
-                                  horizontal: 20,
-                                ),
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    ' ${currentUser.name}',
-                                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                                PaddedRow(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                    horizontal: 20,
                                   ),
-                                  Row(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text.rich(
-                                            TextSpan(
-                                              text: '${currentUser.followers.length} ',
-                                              children: const [
-                                                TextSpan(
-                                                  text: 'followers',
-                                                  style: TextStyle(
-                                                    color: Pallete.whiteColor,
-                                                    fontWeight: FontWeight.bold,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      ' ${currentUser.name}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text.rich(
+                                              TextSpan(
+                                                text:
+                                                    '${currentUser.followers.length} ',
+                                                children: const [
+                                                  TextSpan(
+                                                    text: 'followers',
+                                                    style: TextStyle(
+                                                      color: Pallete.whiteColor,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
                                                   ),
-                                                ),
-                                              ],
+                                                ],
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                      const Gap(10),
-                                      Row(
-                                        children: [
-                                          Text.rich(
-                                            TextSpan(
-                                              text: '${currentUser.following.length} ',
-                                              children: const [
-                                                TextSpan(
-                                                  text: 'following',
-                                                  style: TextStyle(
-                                                    color: Pallete.whiteColor,
-                                                    fontWeight: FontWeight.bold,
+                                          ],
+                                        ),
+                                        const Gap(10),
+                                        Row(
+                                          children: [
+                                            Text.rich(
+                                              TextSpan(
+                                                text:
+                                                    '${currentUser.following.length} ',
+                                                children: const [
+                                                  TextSpan(
+                                                    text: 'following',
+                                                    style: TextStyle(
+                                                      color: Pallete.whiteColor,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
                                                   ),
-                                                ),
-                                              ],
+                                                ],
+                                              ),
                                             ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              const Gap(30),
-                              Expanded(
-                                child: ListView.builder(
-                                  padding: EdgeInsets.zero,
-                                  itemCount: actionMaps.length,
-                                  itemBuilder: (context, index) {
-                                    final (icon, title) = actionMaps[index];
-                                    return ListTile(
-                                      leading: Icon(icon),
-                                      title: Text(title),
-                                    );
-                                  },
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
-                              ),
-                            ],
+                                const Gap(30),
+                                Expanded(
+                                  child: ListView.builder(
+                                    padding: EdgeInsets.zero,
+                                    itemCount: actionMaps.length,
+                                    itemBuilder: (context, index) {
+                                      final (icon, title) = actionMaps[index];
+                                      return ListTile(
+                                        leading: Icon(icon),
+                                        title: Text(title),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
--- phenikaa_campus/macos/Flutter/GeneratedPluginRegistrant.swift
+++ phenikaa_campus/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,7 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
